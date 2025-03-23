ce60f571c410f2216f1ff55c80b634cb3095b9bc
@hexlet/cli <support@hexlet.io>
2025-03-23 13:15:20 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/app/javascript/application.js
+++ rails-basics-ru/crud/app/javascript/application.js
@@ -1,7 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import Rails from "@rails/ujs"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
-Rails.start()
